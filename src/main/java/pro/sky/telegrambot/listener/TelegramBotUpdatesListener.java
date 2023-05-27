a66893d0603fa1c5a4df15f1ccb078e38d78413e
Viking <djviking@yandex.ru>
2023-05-27 16:06:35 +0300

    сделали шедуллер заработало :) и напоминалка через минуту

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -50,20 +50,11 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             if (msg.equals("/start")) {
                 SendMessage message = new SendMessage(
                         update.message().chat().id(),
-                        String.format("Здравствуйте %s, вы зарегистрировались в моей сети. Напишите (Задача), чтоб получить указания или (love), чтоб узнать кто кого любит.", update.message().chat().firstName())
+                        String.format("Здравствуйте %s, вы зарегистрировались в моей сети. Напишите /Задача, чтоб получить указания или /love, чтоб узнать кто кого любит. Введите задачу в формате dd.MM.yyyy HH:mm Задача (пример: 12.02.2023 10:45 Сходить в магазин.)", update.message().chat().firstName())
                 );
                 logger.info("Start button has been activated ^)");
                 telegramBot.execute(message);
             }
-//            if (msg.equals("love")) {
-//                SendMessage message = new SendMessage(
-//                        update.message().chat().id(),
-//                        String.format(" s% - ты жопа с ручкой ^), и я тебя ЛЮБЛЮ..", update.message().chat().firstName())
-//                );
-//                logger.info("был забрызнут метод про любовь и ...опу :) - тест версия");
-//                telegramBot.execute(message);
-//            }
-
 
             Pattern pattern = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W\\w+]+)");
             Matcher matcher = pattern.matcher(msg);
@@ -85,56 +76,17 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
-}
 
-//    public int taskProcess(List<Update> updates) {
-//        Pattern pattern = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W\\w+]+)");
-//        Matcher matcher = pattern.matcher(msg);
-//        if(matcher.matches()) {
-//            String dateTimeStr = msg.substring(0, 16);
-//            String text = msg.substring(17);
-//            System.out.println(dateTimeStr);
-//            LocalDateTime dateTime = LocalDateTime.parse(dateTimeStr, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
-//            System.out.println(text);
-//            NotificationTask notificationTask = new NotificationTask(update.message().chat().id().text, dateTime);
-//            notificationTaskRepository.save(notificationTask);
-//    }
 
-    // создаем метод под старт бота - при нажатии /start срабатывает этот метод
-//    private void startMessage(long chatId, String userName) {
-//        SendMessage message = new SendMessage(chatId, "Здравствуйте, вы зарегистрировались в моей сети: " + userName + ". Напишите (/Задача), чтоб получить указания или (love), чтоб узнать кто кого любит.  ");
-//
-//        SendResponse response = telegramBot.execute(message);
-//        if (response.isOk()) {
-//            logger.info("Start button has been activated ^)");
-//        } else {
-//            logger.error("Error sending. Code: " + response.errorCode());
-//
-//        }
-//    }
-//    @Override
-//    private int taskMessage(List<Update> updates) {
-//        updates.forEach(update -> {
-//            logger.info("текст задания: {}", update);
-//            String msg = update.message().text();
-//            if (msg.equals("/Задача")) {
-//                SendMessage message = new SendMessage(
-//                        update.message().chat().id(),
-//                        String.format("Пользователь, %s. Введите задачу в формате dd.mm.yyyy hh:mm Задача (пример: 12.02.2023 10:45 Сходить в магазин)", update.message().chat().firstName())
-//                );
-//                telegramBot.execute(message);
-//            }
-//        });
-//        return UpdatesListener.CONFIRMED_UPDATES_ALL;
-//    }
+//    ниже решение для себя через кейсы и ключевые слова.
 
-//    public int process1(List<Update> updates) {
+//    public int process(List<Update> updates) {
 //        updates.forEach(update -> {
 //            logger.info("Processing update: {}", update);
 //            switch (update.message().text()) {
-////                case "/start":
-////                    startMessage(update.message().chat().id(), update.message().chat().firstName());
-//                break;
+//                case "/start":
+//                    startMessage(update.message().chat().id(), update.message().chat().firstName());
+//                    break;
 //                case "love":
 //                    loveMessage(update.message().chat().id(), update.message().chat().firstName());
 //                    break;
@@ -148,8 +100,9 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 ////            // Process your updates here
 //        });
 //        return UpdatesListener.CONFIRMED_UPDATES_ALL;
-
-    //    выведены методы под ключевые слова
+//    }
+//
+////    выведены методы под ключевые слова
 //    private void taskMessage1(long chatId, String userName) {
 //        SendMessage message = new SendMessage(chatId, "Пользователь - " + userName + ". Введите задачу в формате dd.MM.yyyy HH:mm Задача (пример: 12.02.2023 10:45 Сходить в магазин.)");
 //        SendResponse response = telegramBot.execute((message));
@@ -160,29 +113,31 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 //            logger.error("не был использован метод задачи " + response.errorCode());
 //        }
 //    }
-
+//// создаем метод под старт бота - при нажатии /start срабатывает этот метод
+//    private void startMessage(long chatId, String userName) {
+//        SendMessage message = new SendMessage(chatId, "Здравствуйте, вы зарегистрировались в моей сети: " + userName + ". Напишите (/Задача), чтоб получить указания или (love), чтоб узнать кто кого любит.  ");
+//
+//        SendResponse response = telegramBot.execute(message);
+//        if (response.isOk()) {
+//            logger.info("Start button has been activated ^)");
+//        } else {
+//            logger.error("Error sending. Code: " + response.errorCode());
+//
+//        }
+//    }
 //    private void loveMessage(long chatId, String userName) {
-//        SendMessage message = new SendMessage(chatId, userName + " - ты жопа ^), и я тебя ...:)");
+//        SendMessage message = new SendMessage(chatId, userName + " - ты милая ^), и я тебя ... ЛЮБЛЮ:)");
 //        SendResponse response = telegramBot.execute((message));
 //        if (response.isOk()) {
-//            logger.info("был забрызнут метод про любовь и ...опу :) - тест версия");
+//            logger.info("был  метод про любовь и ...опу :) - тест версия");
 //
 //        } else {
 //            logger.error("не написано про ..опу " + response.errorCode());
 //        }
 //    }
 
-    //    делаем паттерн
-//    Pattern pattern = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W\\w+]+)");
-//    Matcher matcher = pattern.matcher(msg);
-//    if(matcher.matches()) {
-//        String dateTimeStr = msg.substring(0, 16);
-//        String text = msg.substring(17);
-//        System.out.println(dateTimeStr);
-//        LocalDateTime dateTime = LocalDateTime.parse(dateTimeStr, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
-//        System.out.println(text);
-//        NotificationTask notificationTask = new NotificationTask(update.message().chat().id().text, dateTime);
-//        notificationTaskRepository.save(notificationTask);
+}
+
 
 
 
