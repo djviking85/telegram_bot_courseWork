82ecbe4c8bec9ba86ecc1b138dfd48cf3023cb99
Viking <djviking@yandex.ru>
2023-05-29 00:26:23 +0300

    корректим шедулер

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -9,14 +9,18 @@ import org.apache.logging.log4j.message.StringFormattedMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.model.NotificationTask;
 import pro.sky.telegrambot.repository.NotificationTaskRepository;
 
+
 import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,15 +29,20 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
-    public TelegramBotUpdatesListener(TelegramBot telegramBot, NotificationTaskRepository notificationTaskRepository) {
-        this.telegramBot = telegramBot;
-        this.notificationTaskRepository = notificationTaskRepository;
-    }
+//    public TelegramBotUpdatesListener(TelegramBot telegramBot, NotificationTaskRepository notificationTaskRepository) {
+//        this.telegramBot = telegramBot;
+//        this.notificationTaskRepository = notificationTaskRepository;
+//    }
 
     //    @Autowired
     private TelegramBot telegramBot;
 
-    private NotificationTaskRepository notificationTaskRepository;
+    private final NotificationTaskRepository notificationTaskRepository;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot, NotificationTaskRepository notificationTaskRepository) {
+        this.telegramBot = telegramBot;
+        this.notificationTaskRepository = notificationTaskRepository;
+    }
 
     @PostConstruct
     public void init() {
@@ -50,7 +59,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             if (msg.equals("/start")) {
                 SendMessage message = new SendMessage(
                         update.message().chat().id(),
-                        String.format("Здравствуйте %s, вы зарегистрировались в моей сети. Напишите /Задача, чтоб получить указания или /love, чтоб узнать кто кого любит. Введите задачу в формате dd.MM.yyyy HH:mm Задача (пример: 12.02.2023 10:45 Сходить в магазин.)", update.message().chat().firstName())
+                        String.format("Здравствуйте %s, вы зарегистрировались в моей сети.  Введите задачу в формате dd.MM.yyyy HH:mm Задача (пример: 12.02.2023 10:45 Сходить в магазин.)", update.message().chat().firstName())
                 );
                 logger.info("Start button has been activated ^)");
                 telegramBot.execute(message);
@@ -76,65 +85,6 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
-
-
-//    ниже решение для себя через кейсы и ключевые слова.
-
-//    public int process(List<Update> updates) {
-//        updates.forEach(update -> {
-//            logger.info("Processing update: {}", update);
-//            switch (update.message().text()) {
-//                case "/start":
-//                    startMessage(update.message().chat().id(), update.message().chat().firstName());
-//                    break;
-//                case "love":
-//                    loveMessage(update.message().chat().id(), update.message().chat().firstName());
-//                    break;
-//                case "Задача":
-//                    taskMessage1(update.message().chat().id(), update.message().chat().firstName());
-//                    break;
-//                default:
-//                    logger.info("Unexpected");
-//
-//            }
-////            // Process your updates here
-//        });
-//        return UpdatesListener.CONFIRMED_UPDATES_ALL;
-//    }
-//
-////    выведены методы под ключевые слова
-//    private void taskMessage1(long chatId, String userName) {
-//        SendMessage message = new SendMessage(chatId, "Пользователь - " + userName + ". Введите задачу в формате dd.MM.yyyy HH:mm Задача (пример: 12.02.2023 10:45 Сходить в магазин.)");
-//        SendResponse response = telegramBot.execute((message));
-//        if (response.isOk()) {
-//            logger.info("был использован метод задачи");
-//
-//        } else {
-//            logger.error("не был использован метод задачи " + response.errorCode());
-//        }
-//    }
-//// создаем метод под старт бота - при нажатии /start срабатывает этот метод
-//    private void startMessage(long chatId, String userName) {
-//        SendMessage message = new SendMessage(chatId, "Здравствуйте, вы зарегистрировались в моей сети: " + userName + ". Напишите (/Задача), чтоб получить указания или (love), чтоб узнать кто кого любит.  ");
-//
-//        SendResponse response = telegramBot.execute(message);
-//        if (response.isOk()) {
-//            logger.info("Start button has been activated ^)");
-//        } else {
-//            logger.error("Error sending. Code: " + response.errorCode());
-//
-//        }
-//    }
-//    private void loveMessage(long chatId, String userName) {
-//        SendMessage message = new SendMessage(chatId, userName + " - ты милая ^), и я тебя ... ЛЮБЛЮ:)");
-//        SendResponse response = telegramBot.execute((message));
-//        if (response.isOk()) {
-//            logger.info("был  метод про любовь и ...опу :) - тест версия");
-//
-//        } else {
-//            logger.error("не написано про ..опу " + response.errorCode());
-//        }
-//    }
 
 }
 
--- src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
@@ -4,8 +4,12 @@ import com.pengrad.telegrambot.model.Update;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pro.sky.telegrambot.model.NotificationTask;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
 
+    List<NotificationTask> findByDateTime(LocalDateTime now);
 }
+
